fabd4e6a839a89ed85dcb6d0cafee2b3f4540dfa
unknown <Alec@.(none)>
2009-06-02 00:00:26 +0800

    add offshore funds
    Signed-off-by: unknown <Alec@.(none)>

--- fnr.pl
+++ fnr.pl
@@ -16,21 +16,23 @@ use WWW::Mechanize;
 use HTML::TableExtract;
 use Data::TreeDumper;
 use Smart::Comments;
-use List::MoreUtils qw(any);
+use List::MoreUtils qw(any uniq);
 
-use version; our $VERSION = qv('0.0.6');
+use version; our $VERSION = qv('0.0.7');
 
 our %text;
 my $os = $ENV{OS};
 $os eq 'Windows_NT' ? require 'lang/cht_big5.pm' : require 'lang/cht_utf8.pm';
 my $encoding = $text{encoding};
 
-has 'input'   => ( is => 'rw', isa => 'ArrayRef' );
-has 'output'  => ( is => 'rw', isa => 'Str' );
-has 'config'  => ( is => 'rw', isa => 'Str' );
+has 'input'     => ( is => 'rw', isa => 'ArrayRef' );
+has 'output'    => ( is => 'rw', isa => 'Str' );
+has 'config'    => ( is => 'rw', isa => 'Str' );
+has 'id_list'   => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
+has 'company_links'      => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
+has 'company_names'      => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
 has 'previous_file'      => ( is => 'rw', isa => 'Str' );
 has 'previous_directory' => ( is => 'rw', isa => 'Str' );
-has 'id_list' => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
 
 sub new {
     my ($class, %args) = @_;
@@ -50,8 +52,8 @@ sub new {
     my $file = Wx::Menu->new;
     $file->Append( wxID_EXIT, "$text{exit}(&E)" );
 
-	my $wxID_SETUP_INPUT  = 100;
-	my $wxID_SETUP_OUTPUT = 101;
+    my $wxID_SETUP_INPUT  = 100;
+    my $wxID_SETUP_OUTPUT = 101;
     my $setup = Wx::Menu->new;
     $setup->Append( $wxID_SETUP_INPUT,  "$text{input}(&I)" );
     $setup->Append( $wxID_SETUP_OUTPUT, "$text{output}(&O)" );
@@ -100,20 +102,37 @@ sub new {
     $self->SetIcon( Wx::GetWxPerlIcon() );
     Wx::LogMessage(sprintf "%s%s%s!", $text{greeting}, $text{name}, $VERSION->normal);
 
-	$self->config('fnr.cfg');
+    $self->config('fnr.cfg');
 
-	if (! -e $self->config) {
-		q{} > io($self->config); # create a empty file
-		$self->on_setup_input;
-		$self->on_setup_output;
-	}
-	else {
-		# read config
-		my $config = Config::General->new($self->config);
-		my %config = $config->getall;
-		$self->input(ref $config{input} ? $config{input} : [$config{input}]);
-		$self->output($config{output});
-	}
+    if (! -e $self->config) {
+        q{} > io($self->config); # create a empty file
+        $self->on_setup_input;
+        $self->on_setup_output;
+    }
+    else {
+        # read config
+        my $config = Config::General->new($self->config);
+        my %config = $config->getall;
+        $self->input(ref $config{input} ? $config{input} : [$config{input}]);
+        $self->output($config{output});
+    }
+
+    # fetch links and names
+    my @url = ('http://www.funddj.com/y/yb/YP303000.djhtm', 'http://www.funddj.com/y/yb/YP303001.djhtm');
+
+    my $mech = WWW::Mechanize->new;
+    my @company_links;
+
+    foreach my $url (@url) {
+	$mech->get($url);
+	my %data = map { $_->url_abs => $_ } $mech->links;
+	my @keys = uniq grep { /yp02000[0-1]/ } map { $_->url_abs } $mech->links;
+	push @company_links, @data{@keys};
+    }
+
+    my @company_names = map { decode('big5', $_->text) } @company_links;
+    $self->company_links(\@company_links);
+    $self->company_names(\@company_names);
 
     return $self;
 }
@@ -123,47 +142,40 @@ sub new {
 #---------------------------------------------------------------------------
 
 sub on_setup_input {
-	my $self = shift;
+    my $self = shift;
 
-	my $url = 'http://www.funddj.com/y/yb/YP303000.djhtm';
-	#my $url = 'http://www.funddj.com/y/yb/YP303001.djhtm';
+    my @company_names = @{ $self->company_names };
+    my $dialog = Wx::MultiChoiceDialog->new( $self, "$text{fund}$text{company}$text{select}", "$text{select}$text{company}", [@company_names] );
+    my @selected = $self->input ? @{ $self->input } : ();
 
-	my $mech = WWW::Mechanize->new;
-	$mech->get($url);
-	my @company_links = grep { $_->url_abs =~ /yp020000/ } $mech->links;
-	my @company_names = map { decode('big5', $_->text) } @company_links;
+    if (@selected) {
+        $dialog->SetSelections(@selected);
+        Wx::LogMessage( "$text{previous}$text{select}: " );
+        Wx::LogMessage( join ", ", (map { $company_names[$_] } @selected) );
+    }
 
-	my $dialog = Wx::MultiChoiceDialog->new( $self, "$text{fund}$text{company}$text{select}", "$text{select}$text{company}", [@company_names] );
-	my @selected = $self->input ? @{ $self->input } : ();
+    if( $dialog->ShowModal == wxID_CANCEL ) {
+        Wx::LogMessage( "$text{user}$text{cancel}$text{select}" );
+    } else {
+        my @selections = $dialog->GetSelections;
+        Wx::LogMessage( "$text{user}$text{select}: " );
+        Wx::LogMessage( join ", ", (map { $company_names[$_] } @selections) );
+        $self->input(\@selections);
 
-	if (@selected) {
-		$dialog->SetSelections(@selected);
-		Wx::LogMessage( "$text{previous}$text{select}: " );
-		Wx::LogMessage( join ", ", (map { $company_names[$_] } @selected) );
-	}
+        my $config = Config::General->new($self->config);
+        my %config = $config->getall;
+        $config{input} = $self->input;
+        $config->save_file($self->config, \%config);
+    }
 
-	if( $dialog->ShowModal == wxID_CANCEL ) {
-		Wx::LogMessage( "$text{user}$text{cancel}$text{select}" );
-	} else {
-		my @selections = $dialog->GetSelections;
-		Wx::LogMessage( "$text{user}$text{select}: " );
-		Wx::LogMessage( join ", ", (map { $company_names[$_] } @selections) );
-		$self->input(\@selections);
-
-		my $config = Config::General->new($self->config);
-		my %config = $config->getall;
-		$config{input} = $self->input;
-		$config->save_file($self->config, \%config);
-	}
-
-	$dialog->Destroy;
+    $dialog->Destroy;
 }
 
 sub on_setup_output {
-	my $self = shift;
+    my $self = shift;
 
     my $dialog = Wx::FileDialog->new(
-		$self, "$text{output}$text{setup}", $self->previous_directory || q{},
+        $self, "$text{output}$text{setup}", $self->previous_directory || q{},
         $self->previous_file || q{},
         'Plain text files (*.txt)|*.txt|All files (*.*)|*.*',
         wxFD_OPEN );
@@ -172,14 +184,14 @@ sub on_setup_output {
         my $path = $dialog->GetPath;
 
         if( $path ) {
-			Wx::LogMessage("$text{output}$text{setup}");
-			Wx::LogMessage("$path");
-			$self->output($path);
+            Wx::LogMessage("$text{output}$text{setup}");
+            Wx::LogMessage("$path");
+            $self->output($path);
 
-			my $config = Config::General->new($self->config);
-			my %config = $config->getall;
-			$config{output} = $self->output;
-			$config->save_file($self->config, \%config);
+            my $config = Config::General->new($self->config);
+            my %config = $config->getall;
+            $config{output} = $self->output;
+            $config->save_file($self->config, \%config);
         }
 
         $self->previous_directory( $dialog->GetDirectory );
@@ -197,23 +209,23 @@ sub on_run {
 
     Wx::LogMessage("$text{exec}$text{ing}...");
 
-	my $thr = threads->create( sub { 
-		$self->retrieve_nav;
-	} );
+    my $thr = threads->create( sub { 
+        $self->retrieve_nav;
+    } );
 
-	if ( $ENV{OS} ne 'Windows_NT' ) {
-		$thr->join;
-	}
+    if ( $ENV{OS} ne 'Windows_NT' ) {
+        $thr->join;
+    }
 
-	# <Problem>
-	# ubuntu:
-	# 1. without use threads, it will get stuck until parse and retrieve finished
-	# 2. use threads and join will get the same result
-	# 3. use threads but not join sometimes can't show full message on log window
+    # <Problem>
+    # ubuntu:
+    # 1. without use threads, it will get stuck until parse and retrieve finished
+    # 2. use threads and join will get the same result
+    # 3. use threads but not join sometimes can't show full message on log window
 
-	# windows:
-	# 1. looks fine even without threads, but can't move window when exec
-	# 2. use threads and join will hang up
+    # windows:
+    # 1. looks fine even without threads, but can't move window when exec
+    # 2. use threads and join will hang up
 
     return;
 }
@@ -225,52 +237,48 @@ sub retrieve_nav {
     "$text{label}\n" >> io($output);
     Wx::LogMessage($text{label});
 
-	my $url = 'http://www.funddj.com/y/yb/YP303000.djhtm';
-	#my $url = 'http://www.funddj.com/y/yb/YP303001.djhtm';
+    my $mech = WWW::Mechanize->new;
+    my @company_links = @{ $self->company_links };
 
-	my $mech = WWW::Mechanize->new;
-	$mech->get($url);
-	my @company_links = grep { $_->url_abs =~ /yp020000/ } $mech->links;
+    my $index = -1;
+    my @company_selections = @{$self->input};
+    foreach my $company_link (@company_links) {
+        $index++;
+        next unless any { $_ == $index } @company_selections;
+        my $company_name = $company_link->text;
+        my $company_url  = $company_link->url_abs;
+        $mech->get($company_url);
+        
+        my @fund_links = grep { $_->url_abs =~ /yp01000[0-1]/ } $mech->links;
 
-	my $index = -1;
-	my @company_selections = @{$self->input};
-	foreach my $company_link (@company_links) {
-		$index++;
-		next unless any { $_ == $index } @company_selections;
-		my $company_name = $company_link->text;
-		my $company_url  = $company_link->url_abs;
-		$mech->get($company_url);
-		
-		my @fund_links = grep { $_->url_abs =~ /yp010000/ } $mech->links;
+        foreach my $fund_link (@fund_links) {
+            my $fund_name = $fund_link->text;
+            my $log_fund_name = decode('big5', $fund_name);
+            my $fund_url  = $fund_link->url_abs;
+            $mech->get($fund_url);
+            my $content = $mech->content;
 
-		foreach my $fund_link (@fund_links) {
-			my $fund_name = $fund_link->text;
-			my $log_fund_name = decode('big5', $fund_name);
-			my $fund_url  = $fund_link->url_abs;
-			$mech->get($fund_url);
-			my $content = $mech->content;
+            my $te = HTML::TableExtract->new;
+            $te->parse($content);
+            my @tables = $te->tables;
+            my $table= $tables[3];
+            my @values = @{ $tables[3]->rows->[1] }[0..2];
+            my $result = sprintf "%-50s %-15s %-10s %-10s", 
+                $fund_name, 
+                $values[0], 
+                $values[1], 
+                $values[2];
+            "$result\n" >> io($output);
 
-			my $te = HTML::TableExtract->new;
-			$te->parse($content);
-			my @tables = $te->tables;
-			my $table= $tables[3];
-			my @values = @{ $tables[3]->rows->[1] }[0..2];
-			my $result = sprintf "%-50s %-15s %-10s %-10s", 
-				$fund_name, 
-				$values[0], 
-				$values[1], 
-				$values[2];
-			"$result\n" >> io($output);
-
-			my $log_result = sprintf "%-50s %-15s %-10s %-10s", 
-				$log_fund_name, 
-				$values[0], 
-				$values[1], 
-				$values[2];
-			Wx::LogMessage($log_result);
-		}
-		"\n" >> io($output);
-	}
+            my $log_result = sprintf "%-50s %-15s %-10s %-10s", 
+                $log_fund_name, 
+                $values[0], 
+                $values[1], 
+                $values[2];
+            Wx::LogMessage($log_result);
+        }
+        "\n" >> io($output);
+    }
 
     Wx::LogMessage($text{output});
     Wx::LogMessage($self->output);
